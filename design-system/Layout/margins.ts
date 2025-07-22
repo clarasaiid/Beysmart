b076770fba9ad029f73c04747860dce55b6e299a
farah <f.khaled2243@nu.edu.eg>
2025-07-22 17:36:30 +0300

    layout

---
+++ design-system/Layout/margins.ts
@@ -0,0 +1,10 @@
+import { Spacing } from './spacing';
+
+export const Margin = {
+  betweenSections: {
+    marginTop: Spacing.md,
+  },
+  betweenComponents: {
+    marginVertical: Spacing.xs,
+  },
+};
